6de7dd21fb89290f89e78fddfa0e328a0bea78dd
Walter Wang <csw.dawm@gmail.com>
2016-07-12 22:54:12 +0800

    Update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,10 @@ import rx.functions.Action1;
 import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -16,6 +19,17 @@ import java.util.stream.Stream;
  * Created by chenshuwang on 2016/7/11.
  */
 public class Main {
+    public static void println(String... args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.print(args[i]);
+        }
+        System.out.println("");
+    }
+
+    public static void println() {
+        System.out.println("");
+    }
+
     public static void main(String[] args) {
         /*
           Observable.from()
@@ -23,6 +37,8 @@ public class Main {
         */
         Observable.from(Stream.of(1, 2, 3, 4, 5).collect(Collectors.toSet()));
         Observable.from(Arrays.asList(1, 2, 3, 4, 5));
+
+        println();
 
         //create
         Observable.create(new Observable.OnSubscribe<Integer>() {
@@ -36,19 +52,20 @@ public class Main {
         .subscribe(new Subscriber<String>() {
             @Override
             public void onCompleted() {
-                System.out.println("Sequence complete.");
+                println("Sequence complete.");
             }
 
             @Override
             public void onError(Throwable error) {
-                System.out.println("Error encountered: " + error.getMessage());
+                println("Error encountered: " + error.getMessage());
             }
 
             @Override
             public void onNext(String item) {
-                System.out.println("(create) Next: " + item);
+                println("(create) Next: " + item);
             }
         });
+        println();
 
         // from
         /*
@@ -58,32 +75,34 @@ public class Main {
                 new Action1<Integer>() {
                     @Override
                     public void call(Integer item) {
-                        System.out.println("(from) Next: " + item);
+                        println("(from) Next: " + item);
                     }
                 },
                 new Action1<Throwable>() {
                     @Override
                     public void call(Throwable error) {
-                        System.out.println("Error encountered: " + error.getMessage());
+                        println("Error encountered: " + error.getMessage());
                     }
                 },
                 new Action0() {
                     @Override
                     public void call() {
-                        System.out.println("Sequence complete");
+                        println("Sequence complete");
                     }
                 }
             );
          */
+
         Integer[] items = {1, 2, 3, 4, 5};
         Observable myObservable = Observable.from(items);
         myObservable
         .map(i -> String.valueOf((int)i*2))
         .subscribe(
-            item -> System.out.println("(from) Next:" + item),
-            error -> System.out.println("Error encountered: " + error),
-            () -> System.out.println("Sequence complete.")
+            item -> println("(from) Next:" + item),
+            error -> println("Error encountered: " + error),
+            () -> println("Sequence complete.")
         );
+        println();
 
         // just
         Observable.just(1, 2, 3, 4, 5)
@@ -91,18 +110,149 @@ public class Main {
         .subscribe(new Subscriber<String>() {
             @Override
             public void onCompleted() {
-                System.out.println("Sequence complete.");
+                println("Sequence complete.");
             }
 
             @Override
             public void onError(Throwable error) {
-                System.err.println("Error: " + error.getMessage());
+                println("Error: " + error.getMessage());
             }
 
             @Override
             public void onNext(String item) {
-                System.out.println("(just) Next: " + item);
+                println("(just) Next: " + item);
             }
         });
+        println();
+
+        // Map-1
+        Observable.just(1, 2, 3, 4, 5)
+        .map(it -> it+3)
+        .subscribe(it -> println("map-1: " + it));
+        println();
+
+        // Map-2
+        Observable.just(1, 2, 3, 4, 5)
+        .map(new Func1<Integer, Integer>() {
+            @Override
+            public Integer call(Integer it) {
+                return it+3;
+            }
+        })
+        .subscribe(new Action1<Integer>() {
+            @Override
+            public void call(Integer it) {
+                println("map-2: " + it);
+            }
+        });
+        println();
+
+        // FlatMap-1
+        Observable.from(Stream.of(1, 2, 3, 4, 5).collect(Collectors.toList()))
+        .flatMap(it -> Observable.just(it+4))
+        .subscribe(
+            it -> println("flatMap-1: " + it)
+        );
+        println();
+
+        // FlatMap-2
+        Observable.from(Arrays.asList(1, 2, 3, 4, 5))
+        .flatMap(new Func1<Integer, Observable<Integer>>() {
+            @Override
+            public Observable<Integer> call(Integer it) {
+                return Observable.just(it+4);
+            }
+        }).subscribe(new Action1<Integer>() {
+            @Override
+            public void call(Integer it) {
+                println("flatMap-2: " + it);
+            }
+        });
+        println();
+
+        // Range
+        Observable.range(1, 5).subscribe(
+            it -> println("Range: " + it)
+        );
+        println();
+
+        // Repeat
+        Observable.range(6, 10).repeat(2).subscribe(
+            it -> println("Repeat: " + it)
+        );
+        println();
+
+        // Buffer-1
+        Observable.range(1, 20).buffer(5)
+        .flatMap(it -> Observable.from(it))
+        .subscribe(it -> println("Buffer-1: " + it));
+        println();
+
+        // Buffer-2
+        Observable.range(1, 100).buffer(20)
+        .subscribe(it -> println("Buffer-2: " + it));
+        println();
+
+        // GroupBy
+        Observable.range(1, 10).groupBy(it -> it%2 ==0)
+        .flatMap(it->it.toList())
+        .subscribe(it -> println("GroupBy: " + it));
+        println();
+
+        // Scan
+        Observable.range(1, 10).scan((sum, it) -> sum += it)
+        .subscribe(it -> println("Scan: " + it));
+        println();
+
+        // Window
+        Observable.range(1, 100).window(20)
+        .flatMap(it -> it.toList())
+        .subscribe(it -> println("Window: " + it));
+        println();
+
+        // Distinct
+        Observable.just(1, 1, 1, 2, 2, 3, 4, 4, 5).distinct()
+        .subscribe(it -> println("Distinct: " + it));
+        println();
+
+        // ElementAt
+        Observable.range(1, 10).elementAt(3)
+        .subscribe(it -> println("ElementAt: " + it));
+        println();
+
+        // Filter
+        Observable.range(1, 10).filter(it -> it%2 ==0)
+        .subscribe(it -> println("Filter: " + it));
+        println();
+
+        // First
+        Observable.range(1, 10).first()
+        .subscribe(it -> println("First: " + it));
+        println();
+
+        // Last
+        Observable.range(1, 10).last()
+        .subscribe(it -> println("Last: " + it));
+        println();
+
+        // Skip
+        Observable.range(1, 10).skip(5)
+        .subscribe(it -> println("Skip: " + it));
+        println();
+
+        // SkipLast
+        Observable.range(1, 10).skipLast(5)
+        .subscribe(it -> println("SkipLast: " + it));
+        println();
+
+        // Take
+        Observable.range(1, 10).take(3)
+        .subscribe(it -> println("Take: " + it));
+        println();
+
+        // TakeLast
+        Observable.range(1, 10).takeLast(3)
+        .subscribe(it -> println("TakeLast: " + it));
+        println();
     }
 }
